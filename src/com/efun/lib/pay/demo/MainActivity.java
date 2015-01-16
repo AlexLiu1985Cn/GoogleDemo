44bd2ff02eb5c30a153b8f38f607f65f2a720625
AlexLiu1985CN <AlexLiu1985CN@gmail.com>
2015-01-16 17:31:28 +0800

    1

---
+++ src/com/efun/lib/pay/demo/MainActivity.java
@@ -0,0 +1,149 @@
+package com.efun.lib.pay.demo;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.drive.Drive;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+public class MainActivity extends FragmentActivity implements
+		ConnectionCallbacks, OnConnectionFailedListener {
+
+	private String TAG = MainActivity.class.toString();
+	private GoogleApiClient mGoogleApiClient;
+
+	// Request code to use when launching the resolution activity
+	private static final int REQUEST_RESOLVE_ERROR = 1001;
+	// Unique tag for the error dialog fragment
+	private static final String DIALOG_ERROR = "dialog_error";
+	// Bool to track whether the app is already resolving an error
+	private boolean mResolvingError = false;
+	private static final String STATE_RESOLVING_ERROR = "resolving_error";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mResolvingError = savedInstanceState != null
+	            && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+		mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Drive.API)
+				.addScope(Drive.SCOPE_FILE).addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).build();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (!mResolvingError) { // more about this later
+			mGoogleApiClient.connect();
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		mGoogleApiClient.disconnect();
+		super.onStop();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		Log.d("alex", TAG + ": onConnectionFailed!");
+		if (result.getErrorCode() == ConnectionResult.API_UNAVAILABLE) {
+			Log.d("alex", TAG + ": API_UNAVAILABLE!");
+	    }
+		if (mResolvingError) {
+			// Already attempting to resolve an error.
+			return;
+		} else if (result.hasResolution()) {
+			try {
+				mResolvingError = true;
+				result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+			} catch (SendIntentException e) {
+				// There was an error with the resolution intent. Try again.
+				mGoogleApiClient.connect();
+			}
+		} else {
+			// Show dialog using GooglePlayServicesUtil.getErrorDialog()
+			showErrorDialog(result.getErrorCode());
+			mResolvingError = true;
+		}
+	}
+
+	// The rest of this code is all about building the error dialog
+
+	/* Creates a dialog for an error message */
+	private void showErrorDialog(int errorCode) {
+		// Create a fragment for the error dialog
+		ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+		// Pass the error that should be displayed
+		Bundle args = new Bundle();
+		args.putInt(DIALOG_ERROR, errorCode);
+		dialogFragment.setArguments(args);
+		dialogFragment.show(getSupportFragmentManager(), "errordialog");
+	}
+
+	/* Called from ErrorDialogFragment when the dialog is dismissed. */
+	public void onDialogDismissed() {
+		mResolvingError = false;
+	}
+
+	/* A fragment to display an error dialog */
+	public static class ErrorDialogFragment extends DialogFragment {
+		public ErrorDialogFragment() {
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Get the error code and retrieve the appropriate dialog
+			int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+			return GooglePlayServicesUtil.getErrorDialog(errorCode,
+					this.getActivity(), REQUEST_RESOLVE_ERROR);
+		}
+
+		@Override
+		public void onDismiss(DialogInterface dialog) {
+			((MainActivity) getActivity()).onDialogDismissed();
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		Log.d("alex", TAG + ": onConnected!");
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		Log.d("alex", TAG + ": onConnectionSuspended!");
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_RESOLVE_ERROR) {
+			mResolvingError = false;
+			if (resultCode == RESULT_OK) {
+				// Make sure the app is not already connected or attempting to
+				// connect
+				if (!mGoogleApiClient.isConnecting()
+						&& !mGoogleApiClient.isConnected()) {
+					mGoogleApiClient.connect();
+				}
+			}
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
+	}
+}
